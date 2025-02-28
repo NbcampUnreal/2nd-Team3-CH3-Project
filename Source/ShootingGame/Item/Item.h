35058d1bc65bcdd82290b8f50b08a8c9f3197861
이광섭 <rhkdtjqlove@naver.com>
2025-02-28 21:00:18 +0900

    Add item & item properties

---
+++ Source/ShootingGame/Item/Item.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item/ItemProperty.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+
+class USphereComponent;
+class UWidgetComponent;
+
+UCLASS()
+class SHOOTINGGAME_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItem();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
+	USceneComponent* Scene;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Component")
+	UStaticMeshComponent* StaticMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Component")
+	UUserWidget* Widget;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
+	USphereComponent* Collision;
+	UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = "UI")
+	UWidgetComponent* OverheadWidget;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|ItemProperty")
+	EItemName ItemName;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|ItemProperty")
+	EItemType ItemType;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|ItemProperty")
+	UTexture2D* ItemImage;
+	
+
+protected:
+	virtual void BeginPlay() override;	
+
+	//충돌처리 함수
+	UFUNCTION()
+	void OnOverlapBegin(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
---
+++ Source/ShootingGame/Item/ItemProperty.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemProperty.generated.h"
+
+UENUM(BlueprintType)
+enum class EItemType : uint8
+{
+    Weapon          UMETA(DisplayName = "Weapon", ToolTip = "무기"),
+    Consumable      UMETA(DisplayName = "Consumable", ToolTip = "소모품"),
+    Misc            UMETA(DisplayName = "Miscellaneous", ToolTip = "잡다한")
+};
+
+UENUM(BlueprintType)
+enum class EItemName : uint8
+{
+    GreenHerb       UMETA(DisplayName = "GreenHerb", ToolTip = "체력을 약간 회복시키는 녹색 약초"),
+    RedHerb         UMETA(DisplayName = "RedHerb", ToolTip = "조합하는 다른 약초들의 효과를 증대시키는 빨간 약초. 단독으로 사용할 수 없다."),
+    YellowHerb      UMETA(DisplayName = "YellowHerb", ToolTip = "체력의 최대치를 상승시키는 노란 약초. 어떤 약초를 조합하는가에 따라 상승치가 달라진다. 단독으로 사용할 수 없다."),
+    
+};
